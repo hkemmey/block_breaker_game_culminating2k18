1fdd91fca376afceb74a3b550a6b5e8187e5fc4f
hkemmey <harrison.kemmey@gmail.com>
2018-05-11 00:16:20 -0400

    Add files via upload
    added all the project files

---
+++ Scripts/Ball.cs
@@ -0,0 +1,41 @@
+﻿// BALL SCRIPT
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	private Paddle paddle;
+	private Vector3 paddle_to_ball_distance;
+	private bool has_started = false;
+	private Rigidbody2D b_Rigidbody2D;
+
+
+	// Use this for initialization
+	void Start () {
+		paddle = GameObject.FindObjectOfType<Paddle>();
+		paddle_to_ball_distance = this.transform.position - paddle.transform.position;
+		b_Rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!has_started) {
+			// keeps the ball attached to the paddle until told otherwise
+			this.transform.position = paddle.transform.position + paddle_to_ball_distance; 
+
+			// waits for mouse click to launch ball
+			if(Input.GetMouseButtonDown(0)) {
+				has_started = true;
+				b_Rigidbody2D.velocity = new Vector2 (2f, 10f);
+			}
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		Vector2 tweak_velocity = new Vector2 (Random.Range(0f,.2f), Random.Range(0f, 2f));
+		b_Rigidbody2D.velocity += tweak_velocity; 
+	}
+}
---
+++ Scripts/Brick.cs
@@ -0,0 +1,71 @@
+﻿// BRICKS SCRIPT
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+	public int maxHits;
+	private int timesHit;
+	private LevelManager level_manager;
+	private SpriteRenderer b_sprite_renderer;
+	public static int breakableCount;
+	private bool isBreakable;
+
+	// Use this for initialization
+	void Awake () {
+		breakableCount = 0;
+	}
+	void Start () {
+		timesHit = 0;	
+
+		level_manager = GameObject.FindObjectOfType<LevelManager>();
+		b_sprite_renderer = gameObject.GetComponent<SpriteRenderer>();
+		isBreakable = (gameObject.tag == "Breakable");
+		if(isBreakable) {
+			breakableCount ++;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D (Collision2D col) {
+		
+		if(isBreakable) {
+			Handle_hits();
+		}
+	}
+
+	// TODO Remove method once winning is possible
+
+	void SimulateWin() {
+		level_manager.LoadNextLevel();
+	}
+
+	void Handle_hits () {
+		timesHit ++;
+		if(timesHit >= maxHits) {
+			Destroy(gameObject);
+			breakableCount --;
+			//print(breakableCount);
+			level_manager.BrickDestroyed();
+		}
+		else {
+			Switch_brick_color_when_hit();
+		}
+	}
+
+	void Switch_brick_color_when_hit() {
+		if ((timesHit == 1 && maxHits == 3)) {
+			b_sprite_renderer.color = Color.green;
+		}
+		else if((timesHit == 2 && maxHits == 3) || (timesHit == 1 && maxHits == 2)) {
+			b_sprite_renderer.color = Color.yellow;
+		}
+	}
+
+}
